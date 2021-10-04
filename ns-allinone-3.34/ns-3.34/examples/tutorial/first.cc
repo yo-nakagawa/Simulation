02731dabe52409b4360f13a3098a144042a3033f
yo-nakagawa <yo-nakagawa@ist.osaka-u.ac.jp>
2021-10-04 13:42:36 +0900

    確認

--- ns-allinone-3.34/ns-3.34/examples/tutorial/first.cc
+++ ns-allinone-3.34/ns-3.34/examples/tutorial/first.cc
@@ -27,7 +27,7 @@
 //    point-to-point
 //
  
-using namespace ns3;
+using namespace ns3; //ns3を使用しますよ〜って宣言
 
 NS_LOG_COMPONENT_DEFINE ("FirstScriptExample");
 
@@ -37,42 +37,43 @@ main (int argc, char *argv[])
   CommandLine cmd (__FILE__);
   cmd.Parse (argc, argv);
   
-  Time::SetResolution (Time::NS);
-  LogComponentEnable ("UdpEchoClientApplication", LOG_LEVEL_INFO);
+  Time::SetResolution (Time::NS); //時間の設定
+  LogComponentEnable ("UdpEchoClientApplication", LOG_LEVEL_INFO);//ログの設定
   LogComponentEnable ("UdpEchoServerApplication", LOG_LEVEL_INFO);
 
-  NodeContainer nodes;
+  NodeContainer nodes;  //ノードを定義
   nodes.Create (2);
 
-  PointToPointHelper pointToPoint;
-  pointToPoint.SetDeviceAttribute ("DataRate", StringValue ("5Mbps"));
-  pointToPoint.SetChannelAttribute ("Delay", StringValue ("2ms"));
+  PointToPointHelper pointToPoint; //p2pリンクの設定
+  pointToPoint.SetDeviceAttribute ("DataRate", StringValue ("5Mbps")); //レート設定
+  pointToPoint.SetChannelAttribute ("Delay", StringValue ("2ms"));//p2pリンクを作成する際の感覚
 
-  NetDeviceContainer devices;
+  NetDeviceContainer devices; //上記の設定をノードに適用
   devices = pointToPoint.Install (nodes);
+  //プロトコルスタックを乗せていく
+  InternetStackHelper stack; //インターネットの設定
+  stack.Install (nodes);   //ノードに適用
 
-  InternetStackHelper stack;
-  stack.Install (nodes);
+  Ipv4AddressHelper address;//IPアドレスの適用
+  address.SetBase ("10.1.1.0", "255.255.255.0"); //10.1.1.0からスタート
 
-  Ipv4AddressHelper address;
-  address.SetBase ("10.1.1.0", "255.255.255.0");
+  Ipv4InterfaceContainer interfaces = address.Assign (devices); //ノード間に適用
+  
+  //アプリケーションレベルの設定
+  UdpEchoServerHelper echoServer (9);  //UDPのecho serverアプリ(引数はポート番号)
 
-  Ipv4InterfaceContainer interfaces = address.Assign (devices);
+  ApplicationContainer serverApps = echoServer.Install (nodes.Get (1)); //Nodes1にサーバを適用
+  serverApps.Start (Seconds (1.0));//スタートする時間
+  serverApps.Stop (Seconds (10.0));//終了する時間
 
-  UdpEchoServerHelper echoServer (9);
+  UdpEchoClientHelper echoClient (interfaces.GetAddress (1), 9); //1のIPアドレスとポート番号を教える
+  echoClient.SetAttribute ("MaxPackets", UintegerValue (1)); //シミュレーションで送信可能なパケットの最大数
+  echoClient.SetAttribute ("Interval", TimeValue (Seconds (1.0))); //パケットの送信間隔
+  echoClient.SetAttribute ("PacketSize", UintegerValue (1024));//ペイロードのサイズ
 
-  ApplicationContainer serverApps = echoServer.Install (nodes.Get (1));
-  serverApps.Start (Seconds (1.0));
-  serverApps.Stop (Seconds (10.0));
-
-  UdpEchoClientHelper echoClient (interfaces.GetAddress (1), 9);
-  echoClient.SetAttribute ("MaxPackets", UintegerValue (1));
-  echoClient.SetAttribute ("Interval", TimeValue (Seconds (1.0)));
-  echoClient.SetAttribute ("PacketSize", UintegerValue (1024));
-
-  ApplicationContainer clientApps = echoClient.Install (nodes.Get (0));
-  clientApps.Start (Seconds (2.0));
-  clientApps.Stop (Seconds (10.0));
+  ApplicationContainer clientApps = echoClient.Install (nodes.Get (0));//Nodes0にクライアントを適用
+  clientApps.Start (Seconds (2.0));//スタートする時間
+  clientApps.Stop (Seconds (10.0));//終了する時間
 
   Simulator::Run ();
   Simulator::Destroy ();
